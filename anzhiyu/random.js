6405df19b8ac2d02a82c2631fec2952d7f8a50da
weibq <14106834+wei-baiqiang@user.noreply.gitee.com>
2024-06-20 17:01:48 +0800

    Site updated: 2024-06-20 17:01:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/17/真·第一个博客/","2024/06/18/test-2/","2024/06/19/测试阿里云图床/"];function toRandomPost(){
+var posts=["2024/06/18/test-2/","2024/06/19/测试阿里云图床/","2024/06/17/真·第一个博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
